455fc85cf97096ffaf1362d4a570fded9d214fd6
Emily Behlmann <ebehlmann@gmail.com>
2014-08-19 23:33:40 -0500

    I think it's finally working

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,27 @@ $(document).ready(function() {
 		
 		$('li.list').last().click(function() {
 			$('#list-container').show();
+			$('#list-container ul li').remove();
 			$('#list-container h2').text(newList.name);
 			$('#list-container span').text(newList.name);
+
+			newList.tasks.forEach(function(task) {
+				$('#list-container ul').append('<li>' + task.name + '</li>');
+			});
+			
+			$('form#new-task').off();
+			$('form#new-task').submit(function(event) {
+				event.preventDefault();
+				
+				var taskNameInput = $('input#new-task-name').val();
+				var newTask = { name: taskNameInput };
+				
+				newList.tasks.push(newTask);
+				
+				$('#list-container ul').append('<li>' + newTask.name + '</li>');
+				
+				$('input#new-task-name').val('');
+			});
 		});
 	});
 	
