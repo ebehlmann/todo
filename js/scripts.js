92da4e874dd88130b1bd5d51fb9389f69b29b2f5
Emily Behlmann <ebehlmann@gmail.com>
2014-08-21 08:06:15 -0500

    Added badges on lists and tweaked styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-	$('form#new-list').submit(function(event) {
+	$('form#new-list').submit(function(event) { // create a list object
 		event.preventDefault();
 		
 		var listNameInput = $('input#new-list-name').val();
 		var newList = { name: listNameInput, tasks: [] };
+		var uniqueList = newList.name.toLowerCase();
 		
-		$('ul#list-of-lists').append('<li class="list">' + newList.name + '</li>');
+		$('ul#list-of-lists').append('<li class="list list-group-item" id=' + uniqueList + '>' + newList.name + '<span class="badge">' + newList.tasks.length + '</span>' + '</li>');
 		
 		$('input#new-list-name').val('');
 		
@@ -29,6 +30,12 @@ $(document).ready(function() {
 				newList.tasks.push(newTask);
 				
 				$('#list-container ul').append('<li>' + newTask.name + '</li>');
+				
+				// var selectBadge = "#" + newList.name.toLowerCase() + " .badge";
+				var selectBadge = "#" + uniqueList + " .badge";
+				
+				$(selectBadge).empty();
+				$(selectBadge).text(newList.tasks.length);
 				
 				$('input#new-task-name').val('');
 			});
